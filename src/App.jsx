c4b4b6a5a7fd8c6d2106b0bea594b00ad9becfba
ferryfernandoo <ferryfernando2205@gmail.com>
2025-04-10 21:12:58 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/chatbot" element={<ChatBot />} />
           <Route path="/form" element={<Form />} />
         </Routes>
       </main>
